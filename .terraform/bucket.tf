1f0ba41c046c935c3235bb1925ba1853186ebbd4
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-05-22 23:33:30 +0100

    Update bucket.tf

--- .terraform/bucket.tf
+++ .terraform/bucket.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket" "raw" {
+resource "google_storage_bucket" "github-actions-bucket" {
   project = "vn-cloud-resume-api-challenge"
-  name = "raw"
+  name = "github-actions-bucket"
   force_destroy = false
   uniform_bucket_level_access = true
   location = "us-central1"
