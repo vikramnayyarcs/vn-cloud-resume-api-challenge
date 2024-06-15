ea1ec3aa4881ed668a608ed091fbef39785becaa
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 18:15:34 +0100

    First attempt at creating cloud function.

--- cloudFunction/index.js
+++ cloudFunction/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { Firestore } = require("@google-cloud/firestore");
+
 /**
  * Responds to an HTTP request using data from the request body parsed according
  * to the "content-type" header.
@@ -7,7 +9,15 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.getResumeData = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World";
-  res.status(200).send(message);
+exports.getResumeData = async(req, res) => {
+  const firestore = new Firestore();
+  const document = firestore.doc("resumes/1");
+
+  const doc = await document.get();
+
+  if (!doc) {
+    res.status(404).send("Resume NOT Found")
+  }
+
+  res.status(200).send(JSON.stringify(doc));
 };
