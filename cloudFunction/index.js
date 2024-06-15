2b35ca10224de3f6f8119d2d58d05b90329af99e
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 21:37:08 +0100

    Attempt to pull in resume.

--- cloudFunction/index.js
+++ cloudFunction/index.js
@@ -2,6 +2,10 @@
 
 const { Firestore } = require("@google-cloud/firestore");
 
+const firestore = new Firestore({
+  projectId: "vn-cloud-resume-api-challenge",
+});
+
 /**
  * Responds to an HTTP request using data from the request body parsed according
  * to the "content-type" header.
@@ -9,7 +13,24 @@ const { Firestore } = require("@google-cloud/firestore");
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.getResumeData = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World";
-  res.status(200).send(message);
+exports.getResumeData = async(req, res) => {
+  // let message = req.query.message || req.body.message || "Hello World";
+  // res.status(200).send(message);
+
+  try {
+    const resumeDocRef = firestore
+      .collection("resumes")
+      .doc("1");
+    const resumeDoc = await resumeDocRef.get();
+
+    if (resumeDoc.exists) {
+      const resumeData = resumeDoc.data();
+      res.status(200).json(resumeData);
+    } else {
+      res.status(404).send("Resume data not found");
+    }
+  }
+  catch {
+    res.status(500).send("Something Went Wrong")
+  }
 };
