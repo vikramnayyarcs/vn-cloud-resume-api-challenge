c8d25530a4edf651770b1ac43ed2fbf21aa5d9fe
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 17:38:59 +0100

    JSONENCODE resume data.

--- .terraform/firestore.tf
+++ .terraform/firestore.tf
@@ -10,5 +10,162 @@ resource "google_firestore_document" "resume" {
   database    = google_firestore_database.database.name
   collection  = "resumes"
   document_id = "12"
-  fields      = "{\r\n  \"basics\": {\r\n    \"name\": \"Vikram Nayyar\",\r\n    \"label\": \"Computer Science Graduate, All Round Tech Enthusiast\",\r\n    \"image\": \"https://media.licdn.com/dms/image/D4E03AQEjKLH1r-NJ7A/profile-displayphoto-shrink_400_400/0/1678379873396?e=1723680000&v=beta&t=s6xDT3oT23Rl84KByLRhQ7DaZENu3kYuCccqGuVfIZQ\",\r\n    \"email\": \"vikramnayyarcp@gmail.com\",\r\n    \"phone\": \"PRIVATE PHONE NUMBER\",\r\n    \"url\": \"https://vikramnayyarcs.vercel.app/\",\r\n    \"summary\": \"An insight into Vikram Nayyar\",\r\n    \"location\": {\r\n      \"address\": \"PRIVATE ADDRESS\",\r\n      \"postalCode\": \"PRIVATE POST CODE\",\r\n      \"city\": \"Newcastle Upon Tyne\",\r\n      \"countryCode\": \"UK\",\r\n      \"region\": \"North East\"\r\n    },\r\n    \"profiles\": [\r\n      {\r\n        \"network\": \"Twitter\",\r\n        \"username\": \"john\",\r\n        \"url\": \"https://twitter.com/vikramnayyarcs\"\r\n      }\r\n    ]\r\n  },\r\n  \"work\": [\r\n    {\r\n      \"name\": \"SquaredUp\",\r\n      \"position\": \"Software Development Placement Student\",\r\n      \"url\": \"https://company.com\",\r\n      \"startDate\": \"2022-07-04\",\r\n      \"endDate\": \"2023-07-04\",\r\n      \"summary\": \"JavaScript Developer, embedded within the engineering department.\",\r\n      \"highlights\": [\r\n        \"Helping prepare the product for launch.\",\r\n        \"Attending AWS Summit 2023.\"\r\n      ]\r\n    }\r\n  ],\r\n  \"volunteer\": [\r\n    {\r\n      \"organization\": \"TheNibbleByte\",\r\n      \"position\": \"Volunteer Blogger\",\r\n      \"url\": \"https://www.facebook.com/thenibblebyte/\",\r\n      \"startDate\": \"2020-08-01\",\r\n      \"endDate\": \"2021-03-01\",\r\n      \"summary\": \"Blogger\",\r\n      \"highlights\": [\"Contributing towards their blog.\"]\r\n    }\r\n  ],\r\n  \"education\": [\r\n    {\r\n      \"institution\": \"Newcastle University\",\r\n      \"url\": \"https://www.ncl.ac.uk/\",\r\n      \"area\": \"Computer Science With Industrial Placement\",\r\n      \"studyType\": \"Bachelor\",\r\n      \"startDate\": \"2020-09-01\",\r\n      \"endDate\": \"2024-07-01\",\r\n      \"score\": \"First Class Honours\",\r\n      \"courses\": [\r\n        \"CSC1031 - Fundamentals of Computing\",\r\n        \"CSC1032 - Computer Systems Design and Architecture\",\r\n        \"CSC1033 - Information Storage and Retrieval\",\r\n        \"CSC1034 - Programming Portfolio 1 (Python)\",\r\n        \"CSC1035 - Programming Portfolio 2 (Java)\",\r\n        \"CSC2031 - Security and Programming Paradigms\",\r\n        \"CSC2032 - Algorithms\",\r\n        \"CSC2033 - Software Engineering Team Project\",\r\n        \"CSC2034 - Contemporary Topics\",\r\n        \"CSC2035 - Software Systems Design and Implementation\"\r\n      ]\r\n    }\r\n  ],\r\n  \"awards\": [\r\n    {\r\n      \"title\": \"High School 100% Attendance\",\r\n      \"date\": \"2018-09-01\",\r\n      \"awarder\": \"Gosforth Academy\",\r\n      \"summary\": \"High Attendance\"\r\n    }\r\n  ],\r\n  \"certificates\": [\r\n    {\r\n      \"name\": \"Python For Data Science and Machine learning Bootcamp by Jose Portilla\",\r\n      \"date\": \"2020-11-07\",\r\n      \"issuer\": \"Udemy\",\r\n      \"url\": \"https://certificate.com\"\r\n    }\r\n  ],\r\n  \"skills\": [\r\n    {\r\n      \"name\": \"Web Development\",\r\n      \"level\": \"Improving\",\r\n      \"keywords\": [\r\n        \"HTML\",\r\n        \"CSS\",\r\n        \"JavaScript\",\r\n        \"React JS\",\r\n        \"Next JS\",\r\n        \"Tailwind CSS\",\r\n        \"Node JS\",\r\n        \"TypeScript\",\r\n        \"FireBase\",\r\n        \"GCP\",\r\n        \"Nivo\",\r\n        \"Vercel\",\r\n        \"Stripe\"\r\n      ]\r\n    },\r\n    {\r\n      \"name\": \"Scriptingt\",\r\n      \"level\": \"Improving\",\r\n      \"keywords\": [\"Python\", \"JavaScript\"]\r\n    }\r\n  ],\r\n  \"languages\": [\r\n    {\r\n      \"language\": \"English\",\r\n      \"fluency\": \"Native speaker\"\r\n    }\r\n  ],\r\n  \"interests\": [\r\n    {\r\n      \"name\": \"Travelling\",\r\n      \"keywords\": [\"London\", \"Edinburgh\"]\r\n    },\r\n    {\r\n      \"name\": \"Active Lifestyle\",\r\n      \"keywords\": [\"Badminton\", \"Gym\"]\r\n    }\r\n  ],\r\n  \"references\": [\r\n    {\r\n      \"name\": \"TheNibbleByte\",\r\n      \"reference\": \"He has an incredible attitude and mentality that really shines through in his writing - he is keen to share his thoughts and those thoughts are often inspiring and very well expressed. He clearly has a great passion for computer science, as well as great time management skills that can be seen in the way he has scheduled himself to post once a week, giving him time to properly think about what he’s writing and ensure it is of benefit to anyone who many read. Besides all this, he is an extremely reliable and polite person, willing to help out wherever he can. We couldn’t have asked for more from him and recommend him very highly\"\r\n    }\r\n  ],\r\n  \"projects\": [\r\n    {\r\n      \"name\": \"Next JS ECommerce Store\",\r\n      \"description\": \"Amazon CLONE with Next JS, Stripe, Tailwind, Firebaase\",\r\n      \"highlights\": [\"Completed as part of the Sonny Sangha 5 Day CHallenge.\"],\r\n      \"url\": \"https://project.com/\"\r\n    },\r\n    {\r\n      \"name\": \"Cloud Resume API Challenge\",\r\n      \"description\": \"Amazon CLONE with Next JS, Stripe, Tailwind, Firebaase\",\r\n      \"highlights\": [\"Shoutout Rishab in Cloud for sharing!\"],\r\n      \"url\": \"https://project.com/\"\r\n    },\r\n    {\r\n      \"name\": \"Physical Activity Data Visualisation Platform\",\r\n      \"description\": \"Dissertation project using a wide range of tech\",\r\n      \"highlights\": [\"Dissertation Project\"],\r\n      \"url\": \"https://dissertation-2024.vercel.app/\"\r\n    }\r\n  ]\r\n}\r\n"
+  fields      = jsonencode({
+  "basics": {
+    "name": "Vikram Nayyar",
+    "label": "Computer Science Graduate, All Round Tech Enthusiast",
+    "image": "https://media.licdn.com/dms/image/D4E03AQEjKLH1r-NJ7A/profile-displayphoto-shrink_400_400/0/1678379873396?e=1723680000&v=beta&t=s6xDT3oT23Rl84KByLRhQ7DaZENu3kYuCccqGuVfIZQ",
+    "email": "vikramnayyarcp@gmail.com",
+    "phone": "PRIVATE PHONE NUMBER",
+    "url": "https://vikramnayyarcs.vercel.app/",
+    "summary": "An insight into Vikram Nayyar",
+    "location": {
+      "address": "PRIVATE ADDRESS",
+      "postalCode": "PRIVATE POST CODE",
+      "city": "Newcastle Upon Tyne",
+      "countryCode": "UK",
+      "region": "North East"
+    },
+    "profiles": [
+      {
+        "network": "Twitter",
+        "username": "john",
+        "url": "https://twitter.com/vikramnayyarcs"
+      }
+    ]
+  },
+  "work": [
+    {
+      "name": "SquaredUp",
+      "position": "Software Development Placement Student",
+      "url": "https://company.com",
+      "startDate": "2022-07-04",
+      "endDate": "2023-07-04",
+      "summary": "JavaScript Developer, embedded within the engineering department.",
+      "highlights": [
+        "Helping prepare the product for launch.",
+        "Attending AWS Summit 2023."
+      ]
+    }
+  ],
+  "volunteer": [
+    {
+      "organization": "TheNibbleByte",
+      "position": "Volunteer Blogger",
+      "url": "https://www.facebook.com/thenibblebyte/",
+      "startDate": "2020-08-01",
+      "endDate": "2021-03-01",
+      "summary": "Blogger",
+      "highlights": ["Contributing towards their blog."]
+    }
+  ],
+  "education": [
+    {
+      "institution": "Newcastle University",
+      "url": "https://www.ncl.ac.uk/",
+      "area": "Computer Science With Industrial Placement",
+      "studyType": "Bachelor",
+      "startDate": "2020-09-01",
+      "endDate": "2024-07-01",
+      "score": "First Class Honours",
+      "courses": [
+        "CSC1031 - Fundamentals of Computing",
+        "CSC1032 - Computer Systems Design and Architecture",
+        "CSC1033 - Information Storage and Retrieval",
+        "CSC1034 - Programming Portfolio 1 (Python)",
+        "CSC1035 - Programming Portfolio 2 (Java)",
+        "CSC2031 - Security and Programming Paradigms",
+        "CSC2032 - Algorithms",
+        "CSC2033 - Software Engineering Team Project",
+        "CSC2034 - Contemporary Topics",
+        "CSC2035 - Software Systems Design and Implementation"
+      ]
+    }
+  ],
+  "awards": [
+    {
+      "title": "High School 100% Attendance",
+      "date": "2018-09-01",
+      "awarder": "Gosforth Academy",
+      "summary": "High Attendance"
+    }
+  ],
+  "certificates": [
+    {
+      "name": "Python For Data Science and Machine learning Bootcamp by Jose Portilla",
+      "date": "2020-11-07",
+      "issuer": "Udemy",
+      "url": "https://certificate.com"
+    }
+  ],
+  "skills": [
+    {
+      "name": "Web Development",
+      "level": "Improving",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Next JS",
+        "Tailwind CSS",
+        "Node JS",
+        "TypeScript",
+        "FireBase",
+        "GCP",
+        "Nivo",
+        "Vercel",
+        "Stripe"
+      ]
+    },
+    {
+      "name": "Scriptingt",
+      "level": "Improving",
+      "keywords": ["Python", "JavaScript"]
+    }
+  ],
+  "languages": [
+    {
+      "language": "English",
+      "fluency": "Native speaker"
+    }
+  ],
+  "interests": [
+    {
+      "name": "Travelling",
+      "keywords": ["London", "Edinburgh"]
+    },
+    {
+      "name": "Active Lifestyle",
+      "keywords": ["Badminton", "Gym"]
+    }
+  ],
+  "references": [
+    {
+      "name": "TheNibbleByte",
+      "reference": "He has an incredible attitude and mentality that really shines through in his writing - he is keen to share his thoughts and those thoughts are often inspiring and very well expressed. He clearly has a great passion for computer science, as well as great time management skills that can be seen in the way he has scheduled himself to post once a week, giving him time to properly think about what he’s writing and ensure it is of benefit to anyone who many read. Besides all this, he is an extremely reliable and polite person, willing to help out wherever he can. We couldn’t have asked for more from him and recommend him very highly"
+    }
+  ],
+  "projects": [
+    {
+      "name": "Next JS ECommerce Store",
+      "description": "Amazon CLONE with Next JS, Stripe, Tailwind, Firebaase",
+      "highlights": ["Completed as part of the Sonny Sangha 5 Day CHallenge."],
+      "url": "https://project.com/"
+    },
+    {
+      "name": "Cloud Resume API Challenge",
+      "description": "Amazon CLONE with Next JS, Stripe, Tailwind, Firebaase",
+      "highlights": ["Shoutout Rishab in Cloud for sharing!"],
+      "url": "https://project.com/"
+    },
+    {
+      "name": "Physical Activity Data Visualisation Platform",
+      "description": "Dissertation project using a wide range of tech",
+      "highlights": ["Dissertation Project"],
+      "url": "https://dissertation-2024.vercel.app/"
+    }
+  ]
+}
+)
 }
