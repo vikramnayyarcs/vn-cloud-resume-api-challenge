0fc9a90547c2c0f7537813f38102e70d63059463
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 15:12:30 +0100

    Cloud function blueprint.

---
+++ cloudFunction/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Responds to an HTTP request using data from the request body parsed according
+ * to the "content-type" header.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.getResumeData = (req, res) => {
+  let message = req.query.message || req.body.message || "Hello World";
+  res.status(200).send(message);
+};
