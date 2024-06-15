228f8ddb0b7f646ed55bf27e9b2fd7e606ded824
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 15:43:19 +0100

    Fix Terraform bug.

--- .terraform/firestore.tf
+++ .terraform/firestore.tf
@@ -1,8 +1,3 @@
-resource "google_project" "project" {
-  project_id = "vn-cloud-resume-api-challenge"
-  name       = "vn-cloud-resume-api-challenge"
-}
-
 resource "google_firestore_database" "database" {
   project     = "vn-cloud-resume-api-challenge"
   name        = "cloud-resume-challenge-firestore-database"
@@ -11,7 +6,7 @@ resource "google_firestore_database" "database" {
 }
 
 resource "google_firestore_document" "resume" {
-  project     = google_project.project.project_id
+  project     = "vn-cloud-resume-api-challenge"
   database    = google_firestore_database.database.name
   collection  = "resumes"
   document_id = "12"
