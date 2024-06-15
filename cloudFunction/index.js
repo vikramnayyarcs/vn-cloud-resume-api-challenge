764413411df954161642c515e52fdfc7205a4276
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 20:45:32 +0100

    Revert function code.

--- cloudFunction/index.js
+++ cloudFunction/index.js
@@ -9,15 +9,7 @@ const { Firestore } = require("@google-cloud/firestore");
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.getResumeData = async(req, res) => {
-  const firestore = new Firestore();
-  const document = firestore.doc("resumes/1");
-
-  const doc = await document.get();
-
-  if (!doc) {
-    res.status(404).send("Resume NOT Found")
-  }
-
-  res.status(200).send(JSON.stringify(doc));
+exports.helloWorld = (req, res) => {
+  let message = req.query.message || req.body.message || "Hello World";
+  res.status(200).send(message);
 };
