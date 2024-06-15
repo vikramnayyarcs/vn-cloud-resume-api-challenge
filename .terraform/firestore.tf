c03ee3c1fee0b4ec7c66c4ca588755a066d03f3b
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2024-06-15 15:37:34 +0100

    Revert.

--- .terraform/firestore.tf
+++ .terraform/firestore.tf
@@ -1,19 +1,6 @@
-resource "google_project" "project" {
-  project_id = "vn-cloud-resume-api-challenge"
-  name       = "vn-cloud-resume-api-challenge"
-}
-
 resource "google_firestore_database" "database" {
   project     = "vn-cloud-resume-api-challenge"
-  name        = "cloud-resume-challenge-firestore-database"
+  name        = "(default)"
   location_id = "nam5"
   type        = "FIRESTORE_NATIVE"
-}
-
-resource "google_firestore_document" "resume" {
-  project     = google_project.project.project_id
-  database    = google_firestore_database.database.name
-  collection  = "resumes"
-  document_id = "12"
-  fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
 }
